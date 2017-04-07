852fd8fafd159b1777f8a2a955e509d37e514ea7
Kun Chen <kchen@csie.org>
2017-04-07 23:22:26 +0800

    增加註解

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-var run = function() { console.log('按到了喔 2'); }
+// 宣告一個名字為 run 的函式
+var run = function() {
+    console.log('按到了喔 2');
+}
 
+// 設定 div.btn-one 當發生 click 事件時，執行 run 的函式
 $('div.btn-one').on('click', run)
